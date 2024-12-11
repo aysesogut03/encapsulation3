953f77daeea790fdb1de2d129cea5155f6048a1a
aysesogut03 <aysesogut2003@gmail.com>
2024-12-11 12:34:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            hasta hasta1=new hasta();
+            hasta1.TC = "26239029148";
+            Console.WriteLine("Tc kimlik numaranız:" + hasta1.TC+"*******");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hasta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    public class hasta
+    {
+        private string tc;
+        public string  TC
+        {
+            get
+            {
+                return tc.Substring(0, 4);
+            }
+                set
+            {
+                bool kontrol = false;
+                if(value.Length ==11) 
+                { 
+                    for(int i=0;i<value.Length;i++)
+                    {
+                        bool sayiMi = char.IsNumber(value[i]);
+                        if(sayiMi)
+                        {
+
+                        }
+                        else
+                        {
+                            kontrol= true;
+                            break;
+                        }
+
+                    }
+                    if(kontrol)
+                    {
+                        Console.WriteLine("Tc kimlik numaranızda geçersiz değer bulundu");
+                    }
+                    else
+                    {
+                        tc= value;
+                    }
+                }
+            }
+        
+        
+        }
+    }
+}
